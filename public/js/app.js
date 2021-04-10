21b3313c7ca7e98b7c449f64ff20b2e3311fa76b
rsingh516 <ravikant.singh@dxc.com>
2021-04-10 14:31:52 +0530

    set up app for deployment

--- public/js/app.js
+++ public/js/app.js
@@ -9,8 +9,8 @@ weatherForm.addEventListener("submit", (e) => {
   const location = search.value;
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-
-  fetch("http://localhost:3000/weather?address=" + location).then(
+// http://localhost:3000
+  fetch("/weather?address=" + location).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
